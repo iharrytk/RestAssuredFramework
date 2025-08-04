547e32a49a37f635009f5b6784b060bcd79cd801
Haritha Thiagu Kumar <haritha.vf130@gmail.com>
2025-08-04 16:21:13 -0500

    API Automation Framework RestAssured Folder Structure

---
+++ src/main/java/com/qa/gorest/frameworkexception/APIFrameworkException.java
@@ -0,0 +1,16 @@
+package com.qa.gorest.frameworkexception;
+
+public class APIFrameworkException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public APIFrameworkException(String mesg) {
+		super(mesg);
+	}
+	
+	
+
+}
