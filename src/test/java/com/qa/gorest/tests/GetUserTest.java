dab297a9e942b96e56dd5287cb9f6819a3498aef
Haritha Thiagu Kumar <haritha.vf130@gmail.com>
2025-08-07 23:47:34 -0500

    restclient utility

--- src/test/java/com/qa/gorest/tests/GetUserTest.java
+++ src/test/java/com/qa/gorest/tests/GetUserTest.java
@@ -1,5 +1,22 @@
 package com.qa.gorest.tests;
 
+import org.testng.annotations.Test;
+
+import com.qa.gorest.client.RestClient;
+
 public class GetUserTest {
+	
+	RestClient restClient;
+	
+	@Test
+	public void getAllUsers() {
+		
+		restClient=new RestClient();
+		restClient.get("/public/v2/users", true)
+					.then().log().all()
+						.assertThat()
+						.statusCode(200);
+		
+	}
 
 }
