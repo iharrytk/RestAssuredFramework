8f832f27407a062731625fffacf1f13a0fdfe7c0
Haritha Thiagu Kumar <haritha.vf130@gmail.com>
2025-08-08 17:38:08 -0500

    POST call to create a user

--- src/test/java/com/qa/gorest/tests/CreateUserTest.java
+++ src/test/java/com/qa/gorest/tests/CreateUserTest.java
@@ -1,5 +1,38 @@
 package com.qa.gorest.tests;
 
+import org.testng.annotations.Test;
+
+import com.qa.gorest.client.RestClient;
+import com.qa.gorest.pojo.User;
+import com.qa.gorest.utils.StringUtils;
+
 public class CreateUserTest {
+	
+	
+	RestClient restClient;
+	
+	@Test
+	public void createAUserTest() {
+		//post a user
+		User user=new User("harry", StringUtils.getRandonEmail(), "male", "active");
+		restClient=new RestClient();
+		Integer userid=restClient.post("/public/v2/users", false, "json", user)
+					.then().log().all()
+						.assertThat()
+						.statusCode(201)
+						.and()
+						.extract()
+						.path("id");
+		
+		
+		//get the user that is posted -verify
+		//restClient=new RestClient();
+		restClient.get("/public/v2/users/"+userid, true)
+					.then().log().all()
+						.assertThat()
+						.statusCode(200);
+		
+		
+	}
 
 }
--- src/test/java/com/qa/gorest/tests/GetUserTest.java
+++ src/test/java/com/qa/gorest/tests/GetUserTest.java
@@ -3,19 +3,52 @@ package com.qa.gorest.tests;
 import org.testng.annotations.Test;
 
 import com.qa.gorest.client.RestClient;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GetUserTest {
 	
 	RestClient restClient;
 	
 	@Test
-	public void getAllUsers() {
+	public void getAllUsersTest() {
 		
 		restClient=new RestClient();
 		restClient.get("/public/v2/users", true)
 					.then().log().all()
 						.assertThat()
 						.statusCode(200);
+		
+	}
+	
+	
+	@Test
+	public void getUserTest() {
+		
+		restClient=new RestClient();
+		restClient.get("/public/v2/users/8057885", false)
+					.then().log().all()
+						.assertThat()
+						.statusCode(200)
+						.and()
+						.body("id", equalTo(8057885));
+		
+	}
+	
+	@Test
+	public void getUserWithQueryParamsTest() {
+		
+		restClient=new RestClient();
+		Map<String,String> queryParams=new HashMap<String, String>();
+		queryParams.put("name", "Sharda Asan PhD");
+		restClient.get("/public/v2/users", false, null, queryParams)
+					.then().log().all()
+						.assertThat()
+						.statusCode(200)
+						.and()
+						.body("id", equalTo("8057885"));
 		
 	}
 
