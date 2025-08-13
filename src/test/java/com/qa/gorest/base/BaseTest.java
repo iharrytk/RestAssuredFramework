e1a2b49c1cd38c38c3d68d793774d7562c6494d4
Haritha Thiagu Kumar <haritha.vf130@gmail.com>
2025-08-13 00:29:18 -0500

    Refactoring RestClient class to handle Authorization and baseurl,congif properties and basetest

--- src/test/java/com/qa/gorest/base/BaseTest.java
+++ src/test/java/com/qa/gorest/base/BaseTest.java
@@ -1,5 +1,30 @@
 package com.qa.gorest.base;
 
+import java.util.Properties;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+
+import com.qa.gorest.client.RestClient;
+import com.qa.gorest.configuration.ConfigurationManager;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+
 public class BaseTest {
+	
+	protected Properties prop;
+	protected RestClient restClient;
+	protected ConfigurationManager config;
+	
+	@Parameters({"baseURI"})
+	@BeforeTest
+	public void setUp(String baseURI) {
+		
+		RestAssured.filters(new AllureRestAssured());//generates Reports
+		config=new ConfigurationManager();
+		prop=config.init_prop();
+		restClient=new RestClient(prop, baseURI);
+	}
 
 }
